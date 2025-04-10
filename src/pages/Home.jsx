8492d8b34e055921c8018bc2a6340699b7672709
laksh <kadarisailaxman@gmail.com>
2025-04-10 12:13:10 +0530

    refactor: Remove unnecessary React import from Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { getProducts } from '../api/productAPI';
 import ProductCard from '../components/ProductCard';
 
